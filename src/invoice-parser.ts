8d2677e84698da66d87f2363d150b4d25461f63e
Lucas Trajano <lucast@tatango.com>
2023-03-25 16:33:02 -0300

    Add categories. Request now supports query params.

--- src/invoice-parser.ts
+++ src/invoice-parser.ts
@@ -1,95 +1,106 @@
-import currency from 'currency.js';
-import fs from 'fs';
-import pdf from "pdf-parse"
+import currency from "currency.js";
+import fs from "fs";
+import pdf from "pdf-parse";
+import { Category } from "./categories-types";
+import categories from "./categories.json";
 
-const expensesRegex = new RegExp(/([0-9]+\/[0-9]+[A-Za-z].+)/, 'g')
-const regexPositiveCosts = /BRL([0-9]+\.?[0-9]+,[0-9]+)/
+const expensesRegex = new RegExp(/([0-9]+\/[0-9]+[A-Za-z].+)/, "g");
+const regexPositiveCosts = /BRL([0-9]+\.?[0-9]+,[0-9]+)/;
 const regexNegativeCosts = /BRL-([0-9]+\.?[0-9]+,[0-9]+)/;
-const descRegex = /[0-9]+\/[0-9]+(.+)BRL/
-const dateRegex = /[0-9]+\/[0-9]+/
+const descRegex = /[0-9]+\/[0-9]+(.+)BRL/;
+const dateRegex = /[0-9]+\/[0-9]+/;
 
 function extractDescription(row: string) {
-  return descRegex.exec(row)?.[1].trim()
+  const result = descRegex.exec(row)?.[1].trim();
+  return result ? result : "";
 }
 
 function extractCurrency(row: string) {
-  return currency(row.replace('.', '').replace(',', '.'))
+  return currency(row.replace(".", "").replace(",", "."));
 }
 
 function extractDate(row: string) {
-  return dateRegex.exec(row)?.[0]
+  return dateRegex.exec(row)?.[0];
 }
 
 function sanitize(str: string) {
-  return str.replace(/\0/g, '')
+  return str.replace(/\0/g, "");
 }
 
 function parseExpenses(expenses: Array<string>) {
-  const parsedExpenses = []
+  const parsedExpenses = [];
   for (const exp of expenses) {
-    const match = regexPositiveCosts.exec(exp)
-    const matchNegative = regexNegativeCosts.exec(exp)
+    const match = regexPositiveCosts.exec(exp);
+    const matchNegative = regexNegativeCosts.exec(exp);
     if (match) {
+      const description = extractDescription(exp);
       parsedExpenses.push({
-        description: extractDescription(exp),
+        description: description,
         cost: extractCurrency(match[1]).value,
-        date: extractDate(exp)
-      })
-    }    
+        date: extractDate(exp),
+        category: categorize(description),
+      });
+    }
   }
-  return parsedExpenses
+  return parsedExpenses;
+}
+
+function categorize(place: string): Category {
+  for (const cat of categories.foodDelivery.places) {
+    if (place.toLowerCase().includes(cat.toLowerCase())) {
+      return "FoodDelivery";
+    }
+  }
+  for (const cat of categories.party.places) {
+    if (place.toLowerCase().includes(cat.toLowerCase())) {
+      return "Party";
+    }
+  }
+  for (const cat of categories.transport.places) {
+    if (place.toLowerCase().includes(cat.toLowerCase())) {
+      return "Transport";
+    }
+  }
+  for (const cat of categories.dating.places) {
+    if (place.toLowerCase().includes(cat.toLowerCase())) {
+      return "Dating";
+    }
+  }
+  for (const cat of categories.groceries.places) {
+    if (place.toLowerCase().includes(cat.toLowerCase())) {
+      return "Groceries";
+    }
+  }
+  return "Other";
+}
+
+export function sumValues(items: any) {
+  let finalValue = currency(0);
+  for (const item of items) {
+    finalValue = finalValue.add(item.cost);
+  }
+  return finalValue;
 }
 
 export async function parseInvoice(invoicePath: string) {
+  let dataBuffer = fs.readFileSync(invoicePath);
 
-    let dataBuffer = fs.readFileSync(invoicePath)
+  const expenses: string[] = [];
 
-    const expenses: string[] = []
-
-    await pdf(dataBuffer).then(data => {
-        let match = expensesRegex.exec(data.text)
-        while (match != null) {          
-          if (!match[0].includes('PAGAMENTO')) {
-            expenses.push(sanitize(match[0]))
-          }
-          
-          match = expensesRegex.exec(data.text)
+  await pdf(dataBuffer)
+    .then((data) => {
+      let match = expensesRegex.exec(data.text);
+      while (match != null) {
+        if (!match[0].includes("PAGAMENTO")) {
+          expenses.push(sanitize(match[0]));
         }
-    }).catch(e => console.error(e))
 
+        match = expensesRegex.exec(data.text);
+      }
+    })
+    .catch((e) => console.error(e));
 
-    return parseExpenses(expenses)
-
-    
-    // const costs = []
-    // const negativeCosts = []
-
-    // for (const exp of expenses) {        
-        
-    //     const match = regexPositiveCosts.exec(exp);
-    //     const matchNegative = regexNegativeCosts.exec(exp);
-
-    //     if (match) {          
-    //       const price = extractCurrency(match[1])
-    //       costs.push(price)
-    //     } else if (matchNegative) {
-    //         const price = extractCurrency(matchNegative[1])
-    //         negativeCosts.push(price)
-    //     }
-    // }
-
-    // const sums = costs.reduce((acc, val) => {
-    //   return acc.add(val);
-    // }, currency(0));
-
-    // const negSum = negativeCosts.reduce((acc, val) => {
-    //   return acc.add(val);
-    // }, currency(0));
-
-    // console.log("POSITIVE: ", sums.value)
-    // console.log("NEGATIVE: ", negSum.value)
-
-    // console.log("TOTAL COST: ", (sums.add(180.40)).subtract(negSum).value)
+  return parseExpenses(expenses);
 }
 
 // 52 CAFE BISTRO COMERCI 25/02
